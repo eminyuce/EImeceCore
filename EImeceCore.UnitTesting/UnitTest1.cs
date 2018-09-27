f39c791998e7f102b7a6a6a2b9892f64d0758ba9
EMIN YUCE <eminyuce@gmail.com>
2018-09-27 10:53:13 -0400

    First Commit

---
+++ EImeceCore.UnitTesting/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EImeceCore.UnitTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
